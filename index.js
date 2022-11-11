a9c42fb1c7a392ebc6c6083c8e1ec3eb32964d5f
David <djung@uda1.com>
2022-11-11 12:28:14 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+window.addEventListener('load', (event) => {
+    var about = document.getElementById("about_link");
+    about.addEventListener('click', () => {
+        navigate('intro', 0);
+    })
+
+    var experience = document.getElementById("experience_link");
+    experience.addEventListener('click', () => {
+        navigate('experience', -100);
+    })
+
+    var research = document.getElementById("research_link");
+    research.addEventListener('click', () => {
+        navigate('research', -120);
+    })
+    
+    var course = document.getElementById("course_link");
+    course.addEventListener('click', () => {
+        navigate('course', -120);
+    })
+
+    var service = document.getElementById("service_link");
+    service.addEventListener('click', () => {
+        navigate('service', -120);
+    })
+
+    var project = document.getElementById("project_link");
+    project.addEventListener('click', () => {
+        navigate('project', -120);
+    })
+
+    var publication = document.getElementById("publication_link");
+    publication.addEventListener('click', () => {
+        navigate('publication', -120);
+    })
+
+    function navigate(section, offset) {
+        window.scrollTo({top: document.getElementById(section + "_section").offsetTop + offset, left: 0, behavior: 'smooth'});
+    }
+});
